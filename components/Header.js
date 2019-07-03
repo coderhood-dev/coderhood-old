da763692b246daa7cdb630fec003fa70632c136f
Ema Lorenzo <aemalorenzo@gmail.com>
2019-07-03 11:56:21 -0300

    header buttons

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-  root: {
+  container: {
+    flexGrow: 1,
+  },
+  title: {
     flexGrow: 1,
   },
 });
@@ -13,12 +17,14 @@ const useStyles = makeStyles({
 const Header = () => {
   const styles = useStyles();
   return (
-    <div className={styles.root}>
+    <div className={styles.container}>
       <AppBar position="static" color="default">
         <Toolbar>
-          <Typography variant="h6" color="inherit">
+          <Typography variant="h6" color="inherit" className={styles.title}>
             Code Garage
           </Typography>
+          <Button color="inherit">Ingresa</Button>
+          <Button color="inherit">Registrate</Button>
         </Toolbar>
       </AppBar>
     </div>
