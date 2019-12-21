0e65595842555455a97d48d8b0faeab1f6e4a19f
Ema <aemalorenzo@gmail.com>
2019-12-21 19:06:10 -0300

    babel alias module resolver

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-const path = require('path');
-
 module.exports = {
-  webpack(config, options) {
-    config.resolve.alias.components = path.join(__dirname, 'src/components');
-    config.resolve.alias.theme = path.join(__dirname, 'src/theme');
-    return config;
-  },
+  webpack: (config) => config,
 };
