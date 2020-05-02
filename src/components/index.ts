0e168bffd390d60af468700c06a3be9d290d938a
Emanuel Lorenzo <aemalorenzo@gmail.com>
2020-05-02 12:35:28 +0900

    organized imports of components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import ThemeSwitcher from './ThemeSwitcher';
+import Header from './Header';
+import Footer from './Footer';
+import Layout from './Layout';
+import Flex from './Flex';
+
+export { ThemeSwitcher, Header, Footer, Layout, Flex };
