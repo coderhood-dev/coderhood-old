534013bc395943103284cde173d59377f468a39e
David Mason <emamasonn@gmail.com>
2020-08-07 01:25:24 -0300

    create section of jobs

---
+++ pages/jobs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout, Text } from '../components';
+
+const Jobs = ({ posts }) => (
+  <Layout>
+    <Text fontSize="6xl" color="inherit">
+      Developer Jobs
+    </Text>
+  </Layout>
+);
+
+export default Jobs;
