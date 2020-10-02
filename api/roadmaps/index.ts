3c7b3d7824dfaf3bae53d36a5d1c941ce69f8d93
josuezx98 <joosuueerodriguez@gmail.com>
2020-10-02 12:09:27 -0300

    detail roadmap

--- api/roadmaps/index.ts
+++ api/roadmaps/index.ts
@@ -5,12 +5,12 @@ export * from './types';
 
 export const getRoadmaps = () => {
   return fetch<GetRoadmapsResponse>(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/roadmaps`
+    `${process.env.NEXT_PUBLIC_BASE_URL}roadmaps`
   );
 };
 
 export const getRoadmap = (name: string) => {
   return fetch<GetRoadmapResponse>(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/roadmaps/${name}`
+    `${process.env.NEXT_PUBLIC_BASE_URL}roadmaps/${name}`
   );
 };
--- pages/roadmaps/[name].tsx
+++ pages/roadmaps/[name].tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from '@emotion/styled';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import {
   getRoadmaps,
@@ -7,57 +8,176 @@ import {
   Subject,
 } from '../../api/roadmaps';
 import { Layout, Card, Text } from '../../components';
-
-import { Box, Avatar, Collapse, Flex } from '@chakra-ui/core';
-
+import CardTech from '../../components/CardTech';
+import {
+  Link,
+  Flex,
+  Image,
+  Button,
+  useColorMode,
+  Center,
+} from '@chakra-ui/core';
 interface Props {
   roadmap: GetRoadmapResponse;
 }
 
 const Roadmap: React.FC<Props> = ({ roadmap }) => {
   const [expanded, setExpanded] = useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+  const [show, setShow] = React.useState(false);
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Layout>
-      <Text fontSize="6xl" color="inherit">
-        {roadmap.title}
-      </Text>
-      <Text color="inherit">{roadmap.description}</Text>
-      <Flex direction="column" py={16} px={12}>
-        {roadmap.subjects.map((subject: Subject) => (
-          <Card key={subject._id} my={2}>
-            <Box>
-              <Avatar size="md" src={subject.image_url} />
-              {subject.title}
-            </Box>
-            <Box>
-              <Text color="gray.800">{subject.description}</Text>
-            </Box>
-            <Collapse mt={4} isOpen={expanded}>
-              <Box p="6">
-                <Text>Method:</Text>
-                <Text>
-                  Heat 1/2 cup of the broth in a pot until simmering, add
-                  saffron and set aside for 10 minutes.
-                </Text>
-                <Text>
-                  Set aside off of the heat to let rest for 10 minutes, and then
-                  serve.
-                </Text>
-              </Box>
-            </Collapse>
-          </Card>
-        ))}
+    <>
+      <Flex h="90vh" flexDirection="column" w="100%">
+        <Center h="80vh" color="black" w="100%">
+          <Text
+            color={colorMode === 'light' ? 'gray.800' : 'gray.300'}
+            fontFamily="Poppins"
+            fontSize={['2rem', '2rem', '3.5rem', '3.2rem', '4.1rem']}
+            fontWeight="400"
+            w="70%"
+            textAlign="center"
+          >
+            Tu camino como{' '}
+            <span style={{ color: '#f26840' }}>{roadmap.title}</span> empiezá
+            acá.
+          </Text>
+        </Center>
+        <Center
+          h={['20vh', '26vh', '28vh', '28vh', '28vh']}
+          w="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Link
+            href="#roadmap"
+            w="2rem"
+            h={['20vh', '26vh', '26vh', '26vh', '26vh']}
+          >
+            <Image
+              w="2rem"
+              src="../arrow.svg"
+              alt="arrow down"
+              style={{
+                transform: 'rotate(180deg)',
+              }}
+            />
+          </Link>
+          <Image
+            src="../waves.svg"
+            alt="wave up"
+            w="100%"
+            mb={['-0.8px', '-1px', '-1px', '-1px', '-1px']}
+            style={{
+              transform: 'rotate(-180deg)',
+            }}
+            h={['8vh', '26vh', '26vh', '26vh', '26vh']}
+          />
+        </Center>
       </Flex>
-    </Layout>
+      <Center
+        w="100%"
+        h={['30vh', '30vh', '30vh', '40vh', '40vh']}
+        bg="#f26840"
+        color="white"
+      >
+        <Text
+          textAlign="center"
+          color={colorMode === 'light' ? 'gray.800' : 'gray.300'}
+          fontSize={['1rem', '1rem', '1.2rem', '1.4rem', '1.6rem']}
+          fontWeight="400"
+          w="80%"
+        >
+          {roadmap.description}
+        </Text>
+      </Center>
+      <Center
+        id="roadmap"
+        display="flex"
+        flexDirection="column"
+        w="100%"
+        backgroundImage="url('../line.svg')"
+        backgroundRepeat="no-repeat"
+        backgroundPosition="center"
+        backgroundSize={['130vh', '180vh', '180vh', '180vh', '180vh']}
+      >
+        <Flex
+          h={['8vh', '17vh', '24vh', '25vh', '25vh']}
+          w="100%"
+          flexDirection="column"
+          mt={-1}
+        >
+          <Image
+            src="../wave_abajo_rotar.svg"
+            alt="wave down"
+            w="100%"
+            h="25vh"
+            style={{
+              transform: 'rotate(180deg)',
+            }}
+          />
+        </Flex>
+
+        {roadmap.subjects.map((subject: Subject) => (
+          <CardTech
+            image={subject.image_url}
+            title={subject.title}
+            description={subject.description}
+          />
+        ))}
+      </Center>
+      <Center h="70vh" w="100%">
+        <Center
+          h="25vh"
+          w={['80%', '80%', '100%', '100%', '100%']}
+          flexDirection="column"
+          justifyContent="space-around"
+        >
+          <Text
+            color="#f26840"
+            fontFamily="Poppins"
+            fontWeight="700"
+            fontSize={['1.2rem', '1.2rem', '2rem', '2.4rem', '2.8rem']}
+            mb="1rem"
+            textAlign="center"
+          >
+            Estas trabado con algo o necesitas ayuda?
+          </Text>
+          <Text
+            color={colorMode === 'light' ? 'gray.800' : 'gray.300'}
+            fontFamily="Poppins"
+            fontWeight="700"
+            textAlign="center"
+            fontSize={['0.9rem', '0.9rem', '1rem', '1rem', '1.4rem']}
+            mb="1rem"
+          >
+            Sacate las dudas que tengas en nuestra comunidad en discord
+          </Text>
+          <Link
+            href="https://discord.gg/xw2dbyv"
+            width={['4rem', '6rem', '7rem', '8rem']}
+            _hover={{ color: 'gray.300' }}
+            target="_blank"
+          >
+            <Button
+              _hover={{ bg: '#f26840' }}
+              bg="#f26840"
+              borderRadius="0.2rem"
+              color="gray.300"
+              size="md"
+              fontSize="0.9rem"
+              width={['4rem', '6rem', '7rem', '8rem']}
+            >
+              Unite
+            </Button>
+          </Link>
+        </Center>
+      </Center>
+    </>
   );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const roadmaps = []; // await getRoadmaps();
+  const roadmaps = await getRoadmaps();
 
   // Get the paths we want to pre-render based on roadmaps
   const paths = roadmaps.map((roadmap) => `/roadmaps/${roadmap.name}`);
@@ -68,7 +188,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }: any) => {
-  const roadmap = {}; // await getRoadmap(params.name);
+  const roadmap = await getRoadmap(params.name);
   return {
     props: { roadmap },
   };
